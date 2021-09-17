ce452905529cd09b0849dddf66819203cc3d7439
Ben-Fitz <benfitzharris@aol.com>
2021-09-16 23:53:30 -0700

    Ex logic finished
    logic and printing has been finished, unittesting still needed

--- Excercise1.js
+++ Excercise1.js
@@ -11,16 +11,29 @@ console.log("This has run")
         wordArr = word.toLowerCase()
         wordArr = word.split("");
         wordVals = []
+        firstHalf = 0;
+        secondHalf = 0;
+    
         console.log(wordArr[0].charCodeAt())
         wordArr.forEach(element => {
-        
-            wordVals.push(element.charCodeAt())
-
-            
+            wordVals.push((element.charCodeAt()- 96))
         });
         console.log(wordVals);
+        for (let i = 0; i < (wordArr.length/2); i++){
+            firstHalf += wordVals[i]
+            secondHalf += wordVals[wordVals.length - (i+1)]
+            console.log(firstHalf)
+            console.log(secondHalf)
+        }
+        if(firstHalf == secondHalf){
+            console.log("True!")
+        }
+        else{
+            console.log("False...")
+        }
     
     }
+    
 
 input = "ogopogo"
 
