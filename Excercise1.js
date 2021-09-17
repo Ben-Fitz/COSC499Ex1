73663b14e9715a11189d6bde8f37846c32cf5352
Ben-Fitz <benfitzharris@aol.com>
2021-09-16 23:15:20 -0700

    Create Ex1
    create file and print to console

---
+++ Excercise1.js
@@ -0,0 +1,12 @@
+//Balanced Words-Ben Fitzharris
+
+console.log("This has run")
+
+
+    void function Balanced(word){
+        wordArr = word.split("");
+        console.log(wordArr);
+    
+    }
+
+
